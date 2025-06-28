bfec6e7c05daad67a0bacce6a1a2909ee75f8d3a
AjayTeotia <teotiaajay20@gmail.com>
2025-06-28 23:29:14 +0530

    projects

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,8 +1,10 @@
 import { messageRoute } from '@/modules/messages/server/procedure';
 import { createTRPCRouter } from '../init';
+import { projectRoute } from '@/modules/projects/server/procedure';
 
 export const appRouter = createTRPCRouter({
-    message: messageRoute
+    message: messageRoute,
+    projects: projectRoute
 })
 // export type definition of API
 export type AppRouter = typeof appRouter;
